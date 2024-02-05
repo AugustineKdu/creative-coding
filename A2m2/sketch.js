b1b6cb26ef96d53d385cf84eb0fcee822eca7733
Augustine <augustine.kim@griffithuni.edu.au>
2024-02-06 05:08:40 +1000

    Start setting on level1

--- A2m2/sketch.js
+++ A2m2/sketch.js
@@ -30,7 +30,7 @@ class Player {
         this.gravity = 0.5;
         this.lift = -12.5; // -80은 매우 높은 점프 값이므로, 실제 게임 플레이에 적합한 값으로 조정할 필요가 있습니다.
         this.prevY = y;
-        // this.r, this.g, this.b 변수 및 생성자 내 색상 설정 코드를 제거
+
         this.sprite.shapeColor = color(255); // 기본 색상을 흰색 또는 다른 색상으로 설정
     }
 
@@ -43,7 +43,7 @@ class Player {
         if (keyIsDown(LEFT_ARROW)) {
             this.sprite.velocity.x = -7;
             this.sprite.velocity.y = 5;
-            sprite.rotation = 90;
+
         } else if (keyIsDown(RIGHT_ARROW)) {
             this.sprite.velocity.x = 7;
             this.sprite.velocity.y = 5;
@@ -77,9 +77,6 @@ class Player {
     }
 }
 
-
-
-
 function preload() {
 
     // shelters = loadJSON('leaderboard.json');
@@ -88,7 +85,7 @@ function preload() {
 function setup() {
     createCanvas(700, 900);
     state = 'loading';
-    player = new Player(350, 890);
+    player = new Player(350, height - 50);
 
     //create the rgd slider 
     rSlider = createSlider(0, 255, 255, 5);
@@ -103,22 +100,45 @@ function setup() {
     gSlider.hide();
     bSlider.hide();
 
+    // const platformWidth = 50;
+    // const platformHeight = 20;
+    // const numRows = 10;
+    // const numBrickPerRow = width / platformWidth;
+    // const platformSpacing = 150;
+
+    // for (let i = 0; i < numRows; i++) {
+    //     for (let j = 0; j < numBrickPerRow; j++) {
+    //         let x = j * platformWidth + platformWidth / 2;
+    //         let y = i * platformSpacing + platformHeight / 2;
+    //         let platform = new Platform(x, y, platformWidth - 2, platformHeight);
+    //         platforms.push(platform);
+    //     }
+    // }
+
     const platformWidth = 50;
     const platformHeight = 20;
-    const numRows = 7;
+    const totalFloors = 15; // 아래에 1개, 위에 14층 = 총 15층
     const numBrickPerRow = width / platformWidth;
+    const platformSpacing = 150; // 플랫폼 간의 세로 간격
 
-    for (let i = 0; i < numRows; i++) {
+    // 플레이어 바로 아래 시작점 플랫폼 생성
+    platforms.push(new Platform(player.sprite.position.x, player.sprite.position.y + platformHeight + 10, platformWidth, platformHeight));
+
+    // 플레이어 위로 14층의 플랫폼 생성
+    for (let i = 1; i < totalFloors; i++) { // i=1부터 시작하여 바로 아래 플랫폼을 제외한 14층을 생성
         for (let j = 0; j < numBrickPerRow; j++) {
-            let platform = new Platform(j * platformWidth + platformWidth / 2, i * (platformHeight + 100) + platformHeight / 2, platformWidth - 2, platformHeight);
-            platforms.push(platform);
+            let x = j * platformWidth + platformWidth / 2;
+            let y = height - 50 - i * platformSpacing; // 플레이어의 시작점을 기준으로 플랫폼의 Y 좌표 계산
+            platforms.push(new Platform(x, y, platformWidth - 2, platformHeight));
         }
     }
-
-
+    // 좌우 벽 생성 - 플랫폼 높이까지 올라갈 수 있게 조정
+    let wallHeight = totalFloors * platformSpacing; // 벽의 높이를 전체 층수에 맞춰 조정
+    platforms.push(new Platform(25, height - wallHeight / 2, 50, wallHeight)); // 왼쪽 벽
+    platforms.push(new Platform(width - 25, height - wallHeight / 2, 50, wallHeight)); // 오른쪽 벽
     // 좌우 벽 생성
-    platforms.push(new Platform(5, height / 2, 20, height)); // 왼쪽 벽
-    platforms.push(new Platform(width - 5, height / 2, 20, height)); // 오른쪽 벽
+    // platforms.push(new Platform(5, height / 2, 20, height)); // 왼쪽 벽
+    // platforms.push(new Platform(width - 5, height / 2, 20, height)); // 오른쪽 벽
 
     // 아래쪽 벽 생성
     platforms.push(new Platform(width / 2, height - 5, width, 20)); // 아래쪽 벽
@@ -127,7 +147,8 @@ function setup() {
 function drawLevel1() {
     background(220);
     text('Ice Game Level 1', width / 2, height / 2 - 20);
-
+    let yOffset = height / 2 - player.sprite.position.y; // 플레이어가 화면 중앙에 위치하도록 yOffset 계산
+    translate(0, yOffset); // 화면 스크롤
     // 플레이어와 플랫폼 업데이트 및 표시
     player.applyGravity();
     player.move();
@@ -179,6 +200,7 @@ function drawMainMenu() {
     textAlign(CENTER);
     text('Main Menu', width / 2, height / 2 - 50);
     text('Ice Climber Game', width / 2, height / 2 - 20);
+
 
     rSlider.show();
     gSlider.show();
